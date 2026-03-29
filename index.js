c0e3f7512747baa34ea25e691fd2b07a5717fbc6
Jayden <159296700+jjrockjj@users.noreply.github.com>
2026-03-29 18:49:58 -0400

    Created bot and got it working

---
+++ index.js
@@ -0,0 +1,125 @@
+import {Client, Events, Guild, ChannelType, GatewayIntentBits, PermissionFlagsBits } from 'discord.js';
+import {QueryClient} from 'craftping'
+import 'dotenv/config';
+import {readFileSync} from 'node:fs'
+import {writeFile} from 'node:fs/promises'
+
+const token = process.env.DISCORD_TOKEN;
+
+const client = new Client({intents: [GatewayIntentBits.Guilds]});
+
+let statusChannelId = JSON.parse(readFileSync('data.json'))["statusChannelId"];
+
+
+console.log('Status Channel Id: ', statusChannelId);
+
+client.once(Events.ClientReady, (readyClient) => {
+    console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+
+    setInterval(updatePlayerCount, 30000)
+})
+
+//Log in to Discord with Bot's token
+client.login(token);
+let numPlayers = await getPlayers();
+console.log('numPlayers:', numPlayers);
+
+client.on(Events.GuildCreate, async (guild) => {
+    console.log('GuildCreate')
+    let numPlayers = await getPlayers();
+    createStatusChannel(guild, numPlayers);
+})
+
+// client.on(Events.PresenceUpdate, async () => {
+//     console.log('Presence Update')
+//     let numPlayers = await getPlayers();
+//     const guild = client.guilds.cache.get("GUILD_ID")
+//     guild.channels.fetch(statusChannelId)
+//     .then(channel => {
+//         if (!channel) {
+//             createStatusChannel(guild, numPlayers);
+//         } else {
+//             guild.channels.edit(statusChannelId, {name: `Online Players: ${numPlayers}`})
+//             .then(console.log("Updated online players"))
+//         }
+//     })
+// })
+
+function createStatusChannel(guild, numPlayers) {
+    console.log('Create Channel')
+    if (!statusChannelId) {
+        guild.channels.create({
+            name: `Online Players: ${numPlayers}`,
+            type: ChannelType.GuildText,
+            permissionOverwrites: [
+                {
+                    id: guild.roles.everyone,
+                    deny: [
+                        PermissionFlagsBits.AddReactions,
+                        PermissionFlagsBits.ManageChannels,
+                        PermissionFlagsBits.AttachFiles,
+                        PermissionFlagsBits.CreateInstantInvite,
+                        PermissionFlagsBits.SendMessages,
+                        PermissionFlagsBits.SendTTSMessages,
+                        PermissionFlagsBits.ManageMessages,
+                        PermissionFlagsBits.EmbedLinks,
+                        PermissionFlagsBits.MentionEveryone,
+                        PermissionFlagsBits.ManageRoles,
+                        PermissionFlagsBits.ManageWebhooks,
+                        PermissionFlagsBits.UseApplicationCommands,
+                        PermissionFlagsBits.ManageThreads,
+                        PermissionFlagsBits.CreatePublicThreads,
+                        PermissionFlagsBits.CreatePrivateThreads,
+                        PermissionFlagsBits.SendMessagesInThreads,
+                        PermissionFlagsBits.UseEmbeddedActivities,
+                        PermissionFlagsBits.SendVoiceMessages,
+                        PermissionFlagsBits.SendPolls,
+                        PermissionFlagsBits.UseExternalApps,
+                        PermissionFlagsBits.PinMessages,
+                        PermissionFlagsBits.BypassSlowmode,
+                    ]
+                },
+                {
+                    id: client.user.id,
+                    allow: [
+                        PermissionFlagsBits.ManageChannels,
+                        PermissionFlagsBits.SendMessages,
+                        PermissionFlagsBits.BypassSlowmode,
+                        PermissionFlagsBits.ManageMessages,
+                    ]
+                }
+            ]
+        }).then((channel) => {
+            statusChannelId = channel.id
+            writeFile('data.json', `{"statusChannelId": "${statusChannelId}"}`)
+            console.log("Created status channel")
+        })
+    }
+}
+
+async function updatePlayerCount() {
+    console.log('Presence Update')
+    let numPlayers = await getPlayers();
+    const guild = client.guilds.cache.at(0);
+    guild.channels.fetch(statusChannelId)
+    .then(channel => {
+        if (!channel) {
+            createStatusChannel(guild, numPlayers);
+        } else {
+            guild.channels.edit(statusChannelId, {name: `Online Players: ${numPlayers}`})
+            .then(console.log("Updated online players"))
+        }
+    })
+}
+
+async function getPlayers() {
+  let craftClient = new QueryClient();
+
+  let basic = await craftClient.queryBasic('96.230.114.156', 25566, AbortSignal.timeout(5000));
+
+  await craftClient.close();
+
+  console.log('Players: ', basic)
+
+  return basic.numplayers;
+}
